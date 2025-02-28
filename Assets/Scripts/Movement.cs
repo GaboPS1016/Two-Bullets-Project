50688b343295c98791ac4155b9b8e42a20fd22ac
GabrielPS <gabrielperezsuarez1016@gmail.com>
2025-02-28 03:10:00 -0500

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,14 @@ public class Movement : MonoBehaviour
 {
     public GameObject player;
     public Vector3 moves;
-    void Start()
-    {
-        
-    }
-
+    public float speed = 8f;
     void Update()
     {
         moves.x = Input.GetAxis("Horizontal");
         moves.y = Input.GetAxis("Vertical");
         if (moves.x != 0 || moves.y != 0)
-        {
-            player.transform.position += moves;
+        {            
+            player.transform.position += moves * speed * Time.deltaTime;
         }
         
     }
