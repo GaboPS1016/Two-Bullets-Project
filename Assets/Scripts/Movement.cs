16aa870b5d314303e2b73e89a62d5127e5db148b
GabrielPS <gabrielperezsuarez1016@gmail.com>
2025-02-28 03:40:40 -0500

    rotate

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,24 @@ public class Movement : MonoBehaviour
     public GameObject player;
     public Vector3 moves;
     public float speed = 8f;
+    Camera cam;
+    void Start ()
+    {
+        cam = Camera.main;
+    }
     void Update()
     {
+        //Movement
         moves.x = Input.GetAxis("Horizontal");
         moves.y = Input.GetAxis("Vertical");
         if (moves.x != 0 || moves.y != 0)
         {            
             player.transform.position += moves * speed * Time.deltaTime;
         }
-        
+
+        //Rotation
+        Vector2 dir = Input.mousePosition - cam.WorldToScreenPoint(player.transform.position);
+        float ang = Vector2.SignedAngle(Vector2.up, dir);
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
     }
 }
