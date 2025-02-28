5ffd77daec3286e5046e28ceeb9868173d07bf4c
GabrielPS <gabrielperezsuarez1016@gmail.com>
2025-02-28 03:42:38 -0500

    b

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
